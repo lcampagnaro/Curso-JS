ee40520518decc7da6ba9650171c446e44164ccd
Leandro Campagnaro <57913956+lcampagnaro@users.noreply.github.com>
2022-03-01 21:56:56 -0300

    aula

--- EX AULA 15/script.js
+++ EX AULA 15/script.js
@@ -1,1 +1,40 @@
-alert('Olá!')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('Verifique os dados e tente novamente!! - [ERRO] Ano inválido')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+            } else if (idade < 21) {
+                // Jovem
+            } else if (idade < 50) {
+                // Adulto
+            } else {
+                // Idoso
+            }
+        } else
+        if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+            } else if (idade < 21) {
+                // Jovem
+            } else if (idade < 50) {
+                // Adulto
+            } else {
+                // Idoso
+            }
+            res.style.textAlign = 'center'
+            res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        }
+    }
