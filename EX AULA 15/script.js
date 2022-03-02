20cc57aafd8f6bcc286812319b6dcca2793b2c4e
Leandro Campagnaro <57913956+lcampagnaro@users.noreply.github.com>
2022-03-01 22:21:20 -0300

    AULA

--- EX AULA 15/script.js
+++ EX AULA 15/script.js
@@ -15,26 +15,37 @@ function verificar() {
             gênero = 'Homem'
             if (idade >= 0 && idade < 10) {
                 // Criança
+                img.setAttribute('src', 'bebe_M.png')
             } else if (idade < 21) {
                 // Jovem
+                img.setAttribute('src', 'jovem_M.png')
             } else if (idade < 50) {
                 // Adulto
+                img.setAttribute('src', 'Adulto_M.png')
             } else {
                 // Idoso
+                img.setAttribute('src', 'idoso_M.png')
             }
         } else
         if (fsex[1].checked) {
             gênero = 'Mulher'
             if (idade >= 0 && idade < 10) {
                 // Criança
+                img.setAttribute('src', 'bebe_F.png')
             } else if (idade < 21) {
                 // Jovem
+                img.setAttribute('src', 'jovem_F.png')
             } else if (idade < 50) {
                 // Adulto
+                img.setAttribute('src', 'Adulto_F.png')
             } else {
                 // Idoso
+                img.setAttribute('src', 'idoso_F.png')
+
             }
-            res.style.textAlign = 'center'
-            res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
         }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
+}
