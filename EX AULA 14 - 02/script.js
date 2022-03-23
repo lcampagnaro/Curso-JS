2bdd1fec4db0e553e184b03a54c1482d70d1dcf7
Leandro Campagnaro <57913956+lcampagnaro@users.noreply.github.com>
2022-03-22 21:15:07 -0300

    terminando...

--- EX AULA 14 - 02/script.js
+++ EX AULA 14 - 02/script.js
@@ -1,1 +1,35 @@
-alert('Olá!')
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Contagem Impossível -> Faltam dados!'
+    } else {
+        //Transformar em valores numéricos
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo Inválido - Será considerado  Passo = 1')
+            p = 1
+        }
+
+        if (i < f) {
+            // Contagem Crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{270D}`
+            }
+
+        } else {
+            //Contagem Regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{270D}`
+            }
+        }
+        res.innerHTML += `\u{1f3C1}`
+    }
+}
