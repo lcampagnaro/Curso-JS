fdba2f7a7dd67b399c3c7a1236fa18ba34db3684
Leandro Campagnaro <57913956+lcampagnaro@users.noreply.github.com>
2022-04-26 22:32:04 -0300

    Finalizando

--- ANALISADOR DE NÚMEROS/script.js
+++ ANALISADOR DE NÚMEROS/script.js
@@ -21,8 +21,40 @@ function inLista(n, l) {
 
 function adicionar() {
     if (isNumero(num.value) && !inLista(num.value, valores)) {
-        window.alert('Tudo Ok')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
-        window.alert('Valor favorinválido ou já encontrado na lista. Digite outro valor!')
+        window.alert('Valor inválido ou já encontrado na lista. Digite outro valor!')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores, antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados</p>`
+        res.innerHTML += `<p>O maior valor na lista é ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor na lista é ${menor}.</p>`
+        res.innerHTML += `<p>Somando os valores digitados, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média de todos os valores é ${media}.</p>`
     }
 }
